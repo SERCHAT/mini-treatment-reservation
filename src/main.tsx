9b425d27689a65638b420309716ec50b04b2b435
Serchat Chasan <serchatchasan@Serchats-MacBook-Pro.local>
2024-12-11 21:21:49 +0200

    included pages for the site navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home.tsx'
+import About from './pages/about.tsx'
+import Contact from './pages/contact.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>
 )
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function home() {
+    return (
+        <div>
+            <h1>Hello from home page</h1>
+        </div>
+    )
+}
+export default home;
