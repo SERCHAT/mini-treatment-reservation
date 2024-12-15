cedab58cbd99d38a6189889a35b759c67bbe13a7
serchat chasan <serchatchasan@gmail.com>
2024-12-15 22:15:59 +0200

    test

--- src/components/Home/TreatmentList/TreatmentListItem.tsx
+++ src/components/Home/TreatmentList/TreatmentListItem.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Treatment } from '../../../Types/Treatment';
 import '../TreatmentList/treatmentList.css'
 
+
 function TreatmentListItem(props: { treatment: Treatment }) {
+
+
+    const bookNowClicked = useCallback(async (treatment: Treatment) => {
+        //show date picker
+
+    }, [])
+
     return (
         <div className='treatment-list-item'>
+
 
             <div className='tratment-item-img'>
                 <img className='treatment-img' src={props.treatment.imageUrl} />
             </div>
-            <h2 className='treatment-title'>{props.treatment.title}</h2>
-            <p className='treatment-description'>{props.treatment.description}</p>
-            <button className='book-now-button'>Book Now</button>
-        </div>
+            <div className='treatment-text-container'>
+                <h2 className='treatment-title'>{props.treatment.title}</h2>
+                <p className='treatment-description'>{props.treatment.description}</p>
+            </div>
+            <button onClick={() => bookNowClicked(props.treatment)} className='book-now-button'>Book Now</button>
+
+
+
+        </div >
     );
 }
 
