ad58547dbc681a607fba407227621431b7312162
serchat chasan <serchatchasan@gmail.com>
2024-12-15 22:24:18 +0200

    fix

--- src/components/Home/TreatmentList/TreatmentListItem.tsx
+++ src/components/Home/TreatmentList/TreatmentListItem.tsx
@@ -5,24 +5,39 @@ import '../TreatmentList/treatmentList.css'
 
 function TreatmentListItem(props: { treatment: Treatment }) {
 
-
+    const [showDatetimePicker, setShowDatetimePicker] = useState(false)
     const bookNowClicked = useCallback(async (treatment: Treatment) => {
         //show date picker
-
+        setShowDatetimePicker(true)
     }, [])
 
     return (
         <div className='treatment-list-item'>
 
+            {
+                showDatetimePicker == false && (
+                    <>
+                        <div className='tratment-item-img'>
+                            <img className='treatment-img' src={props.treatment.imageUrl} />
+                        </div>
+                        <div className='treatment-text-container'>
+                            <h2 className='treatment-title'>{props.treatment.title}</h2>
+                            <p className='treatment-description'>{props.treatment.description}</p>
+                        </div>
+                        <button onClick={() => bookNowClicked(props.treatment)} className='book-now-button'>Book Now</button>
+                    </>
+                )
+            }
 
-            <div className='tratment-item-img'>
-                <img className='treatment-img' src={props.treatment.imageUrl} />
-            </div>
-            <div className='treatment-text-container'>
-                <h2 className='treatment-title'>{props.treatment.title}</h2>
-                <p className='treatment-description'>{props.treatment.description}</p>
-            </div>
-            <button onClick={() => bookNowClicked(props.treatment)} className='book-now-button'>Book Now</button>
+            {
+                showDatetimePicker == true && (
+                    <>
+                        <input type="datetime-local" />
+
+                    </>
+                )
+            }
+
 
 
 
